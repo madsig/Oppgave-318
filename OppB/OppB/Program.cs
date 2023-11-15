244cc2a991a8623b9e0754e8913c80b99e47c160
madsig <madsig@getacademy.no>
2023-11-15 13:01:31 +0100

    initial commit

---
+++ OppB/OppB/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace OppB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputString = MakeString(args);
+            Console.WriteLine($"Input: {inputString}");
+
+            var reveresString = ReverseString(inputString);
+            Console.WriteLine($"Output: {reveresString}");
+        }
+
+        static string MakeString(string[] array)
+        {
+            var result = "";
+            foreach (var word in array)
+            {
+                result += word + " ";
+            }
+            result = result.Trim();
+            return result;
+        }
+        static string ReverseString(string str)
+        {
+            var result = "";
+            while (str.Length > 0)
+            {
+                result += str[^1];
+                str = str.Remove(str.Length-1, 1);
+            }
+            return result;
+        }
+    }
+}
